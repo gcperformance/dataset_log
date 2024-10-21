bd0d4a2a16b9dac3d0d5a17fe41c63e36f9b75a2
Kyle Fletcher <kyle.fletcher@tbs-sct.gc.ca>
2024-10-21 15:19:41 -0400

    Cleanup.

--- backup_datasets.py
+++ backup_datasets.py
@@ -1,4 +1,3 @@
-#!/bin/env python3
 import os
 import requests
 import json
@@ -11,12 +10,16 @@ services = os.getenv('SERVICES')
 if not services:
     raise EnvironmentError("Environment variable SERVICES is required")
 
+output_dir = "services"
+os.makedirs(output_dir, exist_ok=True)
+
 for service, resource in json.loads(services).items():
+
   url = f"https://open.canada.ca/data/en/datastore/dump/{resource}"
   response = requests.get(url)
   response.raise_for_status()
   
-  csv_file = f"{service}.csv"
+  csv_file = os.path.join(output_dir, f"{service}.csv")
   with open(csv_file, 'wb') as file:
       file.write(response.content)
   
